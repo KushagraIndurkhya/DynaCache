a686f8842b14e501bf539e4c62972ce4ba510195
Kushagra Indurkhya <kushagraindurkhya7@gmail.com>
2023-09-23 16:40:06 +0530

    pickle->ascii string encoding

--- DynaCache/cache.py
+++ DynaCache/cache.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 import logging
 import yaml
@@ -6,20 +5,35 @@ import time
 from botocore.exceptions import BotoCoreError, ClientError
 from functools import wraps
 import pickle
+import binascii
 
 logger = logging.getLogger(__name__)
+
+def serialize_to_ascii(data):
+    serialized_data = pickle.dumps(data)
+    ascii_str = binascii.hexlify(serialized_data).decode("utf-8")
+    return ascii_str
+
+def deserialize_from_ascii(ascii_str):
+    binary_data = binascii.unhexlify(ascii_str.encode("utf-8"))
+    deserialized_data = pickle.loads(binary_data)
+    return deserialized_data
 
 class DynamoDBCache:
     def load_config(self, config_file):
         with open(config_file, "r") as f:
             return yaml.safe_load(f)
-    def __init__(self,cache_table_name,defaultTTL=3600,aws_region=None,config_file=None, aws_access_key_id=None, aws_secret_access_key=None, aws_session_token=None):
-        self.config={}
+
+    def __init__(self, cache_table_name, defaultTTL=3600, aws_region=None, config_file=None, aws_access_key_id=None, aws_secret_access_key=None, aws_session_token=None):
+        self.config = {}
         if config_file:
             self.config = self.load_config(config_file)
-        self.aws_access_key_id = aws_access_key_id or self.config.get("aws_access_key_id")
-        self.aws_secret_access_key = aws_secret_access_key or self.config.get("aws_secret_access_key")
-        self.aws_session_token = aws_session_token or self.config.get("aws_session_token")
+        self.aws_access_key_id = aws_access_key_id or self.config.get(
+            "aws_access_key_id")
+        self.aws_secret_access_key = aws_secret_access_key or self.config.get(
+            "aws_secret_access_key")
+        self.aws_session_token = aws_session_token or self.config.get(
+            "aws_session_token")
         self.aws_region = aws_region or self.config["aws_region"]
         self.cache_table_name = cache_table_name
         self.dynamodb = self.setup_dynamodb()
@@ -28,8 +42,7 @@ class DynamoDBCache:
         except Exception as e:
             raise Exception("Failed to create cache table")
         # self.dynamoTable=self.dynamodb.Table(cache_table_name)
-        self.defaultTTL=defaultTTL
-
+        self.defaultTTL = defaultTTL
 
     def setup_dynamodb(self):
         if not self.aws_access_key_id or not self.aws_secret_access_key:
@@ -57,24 +70,24 @@ class DynamoDBCache:
                     {
                         "AttributeName": "cache_key",
                         "AttributeType": "S"
-                    },
-                    {
-                        "AttributeName": "ttl",
-                        "AttributeType": "N"  # Numeric attribute for TTL
                     }
                 ],
                 ProvisionedThroughput={
                     "ReadCapacityUnits": 5,
                     "WriteCapacityUnits": 5
-                },
-                TimeToLiveSpecification={
-                    "AttributeName": "ttl",  # Specify the TTL attribute name
-                    "Enabled": True           # Enable TTL for the table
                 }
             )
-            self.dynamodb.get_waiter('table_exists').wait(TableName=self.cache_table_name)
+            self.dynamodb.get_waiter('table_exists').wait(
+                TableName=self.cache_table_name)
+            self.dynamodb.update_time_to_live(
+                TableName=self.cache_table_name,
+                TimeToLiveSpecification={
+                    'Enabled': True,
+                    'AttributeName': 'ttl'
+                })
         except self.dynamodb.exceptions.ResourceInUseException:
             pass  # Table already exists
+
     def handle_aws_exceptions(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -85,35 +98,34 @@ class DynamoDBCache:
                 # Handle the error or raise a custom exception here.
         return wrapper
 
-
     def cache_response(self, ttl_seconds=3600):
         def decorator(func):
             @self.handle_aws_exceptions
             @wraps(func)
             def wrapper(*args, **kwargs):
-                key = f"{func.__name__}-{pickle.dumps((args, kwargs))}"
+                # Serialize the input arguments using the reusable function
+                key = serialize_to_ascii((args, kwargs))
                 response = self.dynamodb.get_item(TableName=self.cache_table_name, Key={"cache_key": {"S": key}})
-                
-                # Check if the cached item exists and if its TTL has expired
+                # # Check if the cached item exists and if its TTL has expired
                 if "Item" in response:
                     cached_data = response["Item"]["cached_data"]["S"]
                     ttl = int(response["Item"]["ttl"]["N"])
                     current_time = int(time.time())
-
                     if ttl > current_time:
-                        # Cache is still valid, return the cached data
-                        return pickle.loads(cached_data)
+                        return deserialize_from_ascii(cached_data)
 
-                # If not cached or expired, call the original function
+                # If not cached, call the original function
                 result = func(*args, **kwargs)
                 
-                # Cache the result with a new TTL
+                # Serialize the result using the reusable function
+                serialized_result = serialize_to_ascii(result)
+                # Cache the result with TTL
                 self.dynamodb.put_item(
                     TableName=self.cache_table_name,
                     Item={
                         "cache_key": {"S": key},
-                        "cached_data": {"S": pickle.dumps(result)},
-                        "ttl": {"N": str(current_time + ttl_seconds)}
+                        "cached_data": {"S": serialized_result},
+                        "ttl": {"N": str(int(time.time()) + ttl_seconds)}
                     }
                 )
                 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DynaCache',
-    version='0.1.0.2',
+    version='0.1.0.4',
     description='DynaCache: Effortlessly cache function responses in DynamoDB for lightning-fast, efficient data retrieval.',
     author='Kushagra Indurkhya',
     author_email='kindurkhya7@gmail.com',
@@ -10,6 +10,5 @@ setup(
     packages=['DynaCache'],
     install_requires=[
         'PyYAML>=5.4.1',  # Configuration file parsing
-        'picke>=1.6',  # Object serialization
     ],
 )
